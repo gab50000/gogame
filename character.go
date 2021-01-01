ced7133f43f0215210e964c59baa5b845dea4578
Gabriel Kabbe <gabriel.kabbe@mail.de>
2021-01-01 18:57:56 +0100

    Fix collision detection

--- character.go
+++ character.go
@@ -41,15 +41,15 @@ type Rectangle struct {
 func (r1 Rectangle) collidesWith(r2 Rectangle) bool {
 	if r1.lowerRight.x >= r2.upperLeft.x &&
 		r1.lowerRight.y >= r2.upperLeft.y &&
-		r2.upperLeft.x >= r1.upperLeft.x &&
-		r2.upperLeft.y >= r1.upperLeft.y {
+		r2.upperLeft.x > r1.upperLeft.x &&
+		r2.upperLeft.y > r1.upperLeft.y {
 		return true
 	}
 
 	if r2.lowerRight.x >= r1.upperLeft.x &&
 		r2.lowerRight.y >= r1.upperLeft.y &&
-		r1.upperLeft.x >= r2.upperLeft.x &&
-		r1.upperLeft.y >= r2.upperLeft.y {
+		r1.upperLeft.x > r2.upperLeft.x &&
+		r1.upperLeft.y > r2.upperLeft.y {
 		return true
 	}
 
@@ -81,10 +81,6 @@ func (character *Character) Shield(dir direction) {
 // Attack bla
 func (character *Character) Attack(dir direction) {
 
-}
-
-func (character *Character) getBounds() Rectangle {
-	return character.bounds
 }
 
 // Draw bla
