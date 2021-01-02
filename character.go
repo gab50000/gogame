439e91a515dfefa58a1e3b6cde43db94ad2a9e4b
Gabriel Kabbe <gabriel.kabbe@mail.de>
2021-01-02 10:31:31 +0100

    Refactor collision detection into collision.go

--- character.go
+++ character.go
@@ -34,44 +34,6 @@ type Dimension struct {
 	height int
 }
 
-type Rectangle struct {
-	upperLeft  Position
-	lowerRight Position
-}
-
-/*
- -------------------------------------> x
-| *----------------*
-| |                |
-| |      r1        |
-| |                |
-| |                |
-| |        *-------|--------*
-| |        |       |        |
-| *--------|-------*        |
-|          |      r2        |
-|          |                |
-|          *----------------*
-V y
-*/
-func (r1 Rectangle) collidesWith(r2 Rectangle) bool {
-	if r1.lowerRight.x > r2.upperLeft.x &&
-		r1.lowerRight.y > r2.upperLeft.y &&
-		r2.lowerRight.x > r1.upperLeft.x &&
-		r2.lowerRight.y > r1.upperLeft.y {
-		return true
-	}
-
-	if r2.lowerRight.x > r1.upperLeft.x &&
-		r2.lowerRight.y > r1.upperLeft.y &&
-		r1.lowerRight.x > r2.upperLeft.x &&
-		r1.lowerRight.y > r2.upperLeft.y {
-		return true
-	}
-
-	return false
-}
-
 // Character bla
 type Character struct {
 	bounds        Rectangle
