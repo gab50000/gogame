2de912f4db7b2aa5890591513f3eb8a53c5ba391
Gabriel Kabbe <gabriel.kabbe@mail.de>
2021-02-06 10:17:09 +0100

    Add water

---
+++ water.go
@@ -0,0 +1,40 @@
+package main
+
+type Water struct {
+	tiles []*Tile
+}
+
+type Move struct {
+	posFrom Position
+	posTo   Position
+	dir     direction
+}
+
+func (water Water) getPositions() []Position {
+	positions := make([]Position, 0)
+
+	for _, tile := range water.tiles {
+		positions = append(positions, tile.bounds.lowerRight)
+	}
+	return positions
+}
+
+func (water Water) getFreeFields() []Position {
+	positions := make([]Position, 0)
+	return positions
+
+}
+
+func (water Water) getMoves() []Move {
+	moves := make([]Move, 1)
+
+	return moves
+}
+
+func (water Water) calcEnergy() int {
+	energy := 0
+	for _, tile := range water.tiles {
+		energy += tile.bounds.lowerRight.y
+	}
+	return energy
+}
